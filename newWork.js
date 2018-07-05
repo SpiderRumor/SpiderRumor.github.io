fb1ba8cecabd51557c440e5d17bec97ea83daf21
Nathan Mierski <nmierski@gmail.com>
2018-07-05 17:24:38 -0400

    New Portfolio website

---
+++ newWork.js
@@ -0,0 +1,19 @@
+function filter(name) {
+	var hiddenTerm = name;
+	var sections = document.getElementsByClassName(hiddenTerm);
+	var button = document.getElementById(hiddenTerm);
+	for (var i=0;i < sections.length; i++){
+		if (sections[i].style.display == 'none') {
+			sections[i].style.display = 'block';
+		}
+		else {
+			sections[i].style.display = 'none';
+		};
+	};
+	if (button.style.color == 'black'){
+		button.style.color = 'gray';
+	}
+	else {
+		button.style.color = 'black';
+	}
+};
